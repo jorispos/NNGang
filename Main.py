aeb768beffc682719bb6b6a3fe7b1172f08bab44
jorispos <jorispostmus@gmail.com>
2022-05-21 18:01:33 +0200

    good shit

--- Main.py
+++ Main.py
@@ -1,63 +1,125 @@
-# -*- coding: utf-8 -*-
-"""
-=========================================================
-Linear Regression Example
-=========================================================
-The example below uses only the first feature of the `diabetes` dataset,
-in order to illustrate the data points within the two-dimensional plot.
-The straight line can be seen in the plot, showing how linear regression
-attempts to draw a straight line that will best minimize the
-residual sum of squares between the observed responses in the dataset,
-and the responses predicted by the linear approximation.
+# Yoo lads, I hope this code makes sense, I have tried my best to add as much documentation as possible
+# Try to play around with Henk and get familiar with our soon-to-be super-intelligence
+# if you have any questions feel free to let me know
 
-The coefficients, residual sum of squares and the coefficient of
-determination are also calculated.
-
-"""
-
-# Code source: Jaques Grobler
-# License: BSD 3 clause
-
+# Import Libraries
+from sklearn.neural_network import MLPRegressor
 import matplotlib.pyplot as plt
-import numpy as np
-from sklearn import datasets, linear_model
-from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.patches as mpatches
+import csv
 
-# Load the diabetes dataset
-diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
+# Load the dataset
+file = open('Data/MockData.csv')
+type(file)
 
-# Use only one feature
-diabetes_X = diabetes_X[:, np.newaxis, 2]
+# Extract and store data from dataset
+csvreader = csv.reader(file)
+rows = []
+for row in csvreader:
+    intRow = [int(x) for x in row]
+    rows.append(intRow)
 
-# Split the data into training/testing sets
-diabetes_X_train = diabetes_X[:-20]
-diabetes_X_test = diabetes_X[-20:]
+# Split up all data so that:
+# X = [[1, 2, 3], || y = [4,
+#      [6, 7, 8]] ||     [9]
+X = []
+y = []
+# NOTE: have to manually set this variable to the length of the time series (68 in our case)
+timeLength = 68
+for row in rows:
+    X.append(row[:timeLength-1])
+    y.append(row[timeLength-1])
 
-# Split the targets into training/testing sets
-diabetes_y_train = diabetes_y[:-20]
-diabetes_y_test = diabetes_y[-20:]
+# Length of the amount of data matrices (for x and y)
+XLength = len(y)
 
-# Create linear regression object
-regr = linear_model.LinearRegression()
+# Set to the % of how much data should be used for training
+trainingSplit = 0.8
+trainingSize = round(XLength*trainingSplit)
 
-# Train the model using the training sets
-regr.fit(diabetes_X_train, diabetes_y_train)
+# Split the data into testing and training data (for cross-validation)
+X_train = []
+X_test = []
+y_train = []
+y_test = []
 
-# Make predictions using the testing set
-diabetes_y_pred = regr.predict(diabetes_X_test)
+# Split the actual data into testing and training data
+for rowIndex in range(XLength):
+    # Should the current row be used for training or test?
+    if rowIndex < trainingSize:
+        # Use current row for training
+        X_train.append(X[rowIndex])
+        y_train.append(y[rowIndex])
+    else:
+        # Use current row for testing
+        X_test.append(X[rowIndex])
+        y_test.append(y[rowIndex])
 
-# The coefficients
-print("Coefficients: \n", regr.coef_)
-# The mean squared error
-print("Mean squared error: %.2f" % mean_squared_error(diabetes_y_test, diabetes_y_pred))
-# The coefficient of determination: 1 is perfect prediction
-print("Coefficient of determination: %.2f" % r2_score(diabetes_y_test, diabetes_y_pred))
+# Initialize our MLP Regressor, Henk
+# Currently uses the same parameter as the one in the Nestor example report (except for the hidden layer size)
+henk = MLPRegressor(random_state=1,
+                    max_iter=10000,
+                    hidden_layer_sizes=(5, 5),
+                    solver="lbfgs",
+                    activation="relu",
+                    tol=0.0001,
+                    alpha=0.00001,
+                    )
 
-# Plot outputs
-plt.scatter(diabetes_X_test, diabetes_y_test, color="black")
-plt.plot(diabetes_X_test, diabetes_y_pred, color="blue", linewidth=3)
+# Train the MLPRegressor with the training data (X: input, y: output)
+henk.fit(X_train, y_train)
 
-plt.xticks(())
-plt.yticks(())
+# Predict using our multi-layer perceptron model.
+print("predictions:")
+predictedValues = henk.predict(X_test)
+print(predictedValues)
 
-plt.show()
+# Return the coefficient of determination of the prediction.
+print("score (R^2):")
+print(henk.score(X_test, y_test))
+
+# For illustrative purposes; plot the graph and prediction of the first prediction of the set
+# numPlots is set to the amount of graphs you want to produce
+numPlots = len(X_test)
+
+for i in range(numPlots):
+    plot = plt.figure(i)
+    plt.plot(range(1, timeLength, 1), X_test[i], 'yo')
+    plt.plot(timeLength, y_test[i], 'yo')
+    plt.plot(timeLength, predictedValues[i], 'ro')
+    plt.xlabel('Time')
+    plt.ylabel('Price')
+    plt.title('Henk in action')
+    redLegend = mpatches.Patch(color='red', label='Predicted data')
+    yellowLegend = mpatches.Patch(color='#D5CF0C', label='Actual data')
+    plt.legend(handles=[redLegend, yellowLegend])
+    plt.show()
+
+# Used for Debugging! (can ignore for now, or play around with it)
+# Set the two variables to False/True for matrix debugging/visualization
+debugMatrices = False
+debugMatricesInfo = False
+# Print the appropriate info
+if debugMatricesInfo:
+    print("x-hor length: " + str(len(X[0])))
+    print("x-ver length: " + str(len(X)))
+    print("y-hor length: " + str(len(y)))
+    print("x-hor test length: " + str(len(X_test[0])))
+    print("x-ver test length: " + str(len(X_test)))
+    print("y-hor test length: " + str(len(y_test)))
+    print("x-hor train length: " + str(len(X_train[0])))
+    print("x-ver train length: " + str(len(X_train)))
+    print("y-hor train length: " + str(len(y_train)))
+if debugMatrices:
+    print("x-array:")
+    print(X)
+    print("y-array:")
+    print(y)
+    print("x-train-array")
+    print(X_train)
+    print("y-train-array")
+    print(y_train)
+    print("x-test-array:")
+    print(X_test)
+    print("y-test-array:")
+    print(y_test)
--- MockDataGenerator.py
+++ MockDataGenerator.py
@@ -16,11 +16,15 @@ with open('Data/MockData.csv', 'w', encoding='UTF8', newline='') as f:
     data = []
 
     for col in range(numRows):
+        positiveTrend = random.randint(0, 1)
+        if positiveTrend == 0:
+            positiveTrend = -1
+
         randomStart = random.randint(startRange, endRange)
         rows = []
         for row in range(numCols):
             rows.append(randomStart)
-            randomStart += random.randint(fluctuationStart, fluctuationEnd)
+            randomStart += (random.randint(fluctuationStart, fluctuationEnd))*positiveTrend
         data.append(rows)
 
     writer.writerows(data)
