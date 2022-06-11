427a88731b1b275d77b856d610397274f90ef935
jorispos <jorispostmus@gmail.com>
2022-06-11 15:15:03 +0200

    mock data to utils

--- Program/Utils.py
+++ Program/Utils.py
@@ -1,6 +1,7 @@
 import csv
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import random
 
 
 # Read CSV file and return the integer matrix
@@ -39,3 +40,44 @@ def displayGraphs(x, y, predictions, timeLength):
         yellowLegend = mpatches.Patch(color='#D5CF0C', label='Actual data')
         plt.legend(handles=[redLegend, yellowLegend])
         plt.show()
+
+
+def createMockDataFile(filePath, startRange, endRange, fluctuationStart, fluctuationEnd, numRows, numCols):
+    with open(filePath, 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+
+    data = []
+
+    for col in range(numRows):
+        positiveTrend = random.randint(0, 1)
+        if positiveTrend == 0:
+            positiveTrend = -1
+
+        randomStart = random.randint(startRange, endRange)
+        rows = []
+        for row in range(numCols):
+            rows.append(randomStart)
+            randomStart += (random.randint(fluctuationStart, fluctuationEnd)) * positiveTrend
+        data.append(rows)
+
+    writer.writerows(data)
+
+    print("New .csv file successfully created at: " + filePath + " :)")
+
+
+def createMockData(startRange, endRange, fluctuationStart, fluctuationEnd, numRows, numCols):
+    data = []
+
+    for col in range(numRows):
+        positiveTrend = random.randint(0, 1)
+        if positiveTrend == 0:
+            positiveTrend = -1
+
+        randomStart = random.randint(startRange, endRange)
+        rows = []
+        for row in range(numCols):
+            rows.append(randomStart)
+            randomStart += (random.randint(fluctuationStart, fluctuationEnd)) * positiveTrend
+        data.append(rows)
+
+    return data
