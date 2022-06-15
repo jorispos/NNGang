ea9b5be7e41eb92ca21fe186a708369357adbd38
jorispos <jorispostmus@gmail.com>
2022-06-15 10:05:14 +0200

    terminal

--- Program/Main.py
+++ Program/Main.py
@@ -10,31 +10,35 @@ dataPath = '../Data/subset2.csv'
 trainingSplit = 0.9
 
 # Retrieve the time series matrix from the CSV data
+print("loading data from drive..")
 timeSeriesMatrix = utils.getRows(dataPath)
 
 # Split the data into x and y and train and test
+print("handling and splitting data..")
 data = Data(timeSeriesMatrix, trainingSplit)
 data.handle()
 
 # Create our MLP
+print("initializing Henk..")
 henk = Henk()
 # Train Henk
 henk.MLP.fit(data.X_train, data.y_train)
 
 # Make predictions on testing data and score the algorithm
 predictedValues = henk.MLP.predict(data.X_test)
-print(predictedValues)
 score = henk.MLP.score(data.X_test, data.y_test)
-print(score)
 
 # Visually display the graphs
 # utils.displayGraphs(data.X_test, data.y_test, predictedValues, data.timeLength)
 
+print("begin preprocessing..")
 # Experimental/Debugging for testing
 preprocessed = []
 numTests = 20
 
+# Preprocess the first x : numTests timeseries from the dataset
 for i in range(numTests):
+    print("preprocessing (" + str(i+1) + "/" + str(numTests) + ")..")
     trend = preprocessing.getTrend(timeSeriesMatrix[i])
     detrend = preprocessing.removeTrend(timeSeriesMatrix[i], trend)
     season = preprocessing.getSeasons(detrend)
@@ -44,8 +48,11 @@ for i in range(numTests):
 
 # Scale the data
 scaler = Scaler()
+print("fitting scaler..")
 scaler.fit(preprocessed)
+print("scaling preprocessed data..")
 scaledData = scaler.transform(preprocessed)
+print("plotting scaled preprocessed data..")
 utils.plotDataMatrix(scaledData, 'Scaled ', 16)
 
 # StandardScaler
